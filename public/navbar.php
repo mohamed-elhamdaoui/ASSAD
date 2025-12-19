56a39eb52753e928695d9a9cde53f32882dcb684
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2025-12-19 10:21:53 +0100

    feat: implémentation de la barre de navigation dynamique basée sur les rôles utilisateurs

---
+++ public/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #1a3318 !important; border-bottom: 3px solid #d4af37;">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="index.php">🦁 ASSAD <span style="color:#d4af37">ZOO</span></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto align-items-center">
+                <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="animaux.php">Animaux</a></li>
+                <li class="nav-item"><a class="nav-link" href="visites.php">Visites</a></li>
+                <li class="nav-item"><a class="nav-link" href="lion_atlas.php">Lion de l'Atlas</a></li>
+                <li class="nav-item ms-lg-3">
+                    <a class="btn fw-bold" style="background-color: #c1272d; color: white;" href="login.php">Espace Membre</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
